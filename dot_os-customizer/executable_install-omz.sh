84af05347c4627991f2b58b39e7e526329b3d654
Linus Willner <linus.willner@reaktor.com>
2026-03-12 13:32:40 +0200

    Improve OMZ installer ergonomics

--- dot_os-customizer/executable_install-omz.sh
+++ dot_os-customizer/executable_install-omz.sh
@@ -2,4 +2,7 @@
 
 set -euo pipefail
 
+# Temporarily remove config folder placed there by chezmoi; we will put it back after installation
+rm -rf ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://install.ohmyz.sh)"
+chezmoi apply
